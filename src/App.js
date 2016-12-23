d1feb9e596cb8704e0f8e603e36c74ed85d36366
Tim Overton <timothyoverton@gmail.com>
2016-12-24 00:07:50 +1000

    initial commit based on create-react-app. Renaming of project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React101 - the basics of React</h2>
+          <h3>Boilerplate created with create-react-app</h3>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
